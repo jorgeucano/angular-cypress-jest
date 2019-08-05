332ad573e2d85905475b6843a734978e7744d7c7
jorgeucano <jorgeucano@gmail.com>
2019-08-05 20:50:04 -0300

    add more checks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,19 +1,25 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import { HeroLoaderModule } from '@herodevs/hero-loader';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule, HeroLoaderModule],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
+
+
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-unit-test'`, async(() => {
+  it(`should have as title 'angular-cypress'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('angular-cypress');
